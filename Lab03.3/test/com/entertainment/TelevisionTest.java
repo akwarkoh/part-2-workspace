7e870bfbd86e119cc5c167f5d4d9a25075b03802
akwarkoh <angelakwarko@gmail.com>
2022-08-01 09:24:41 -0700

    Initial commit

---
+++ Lab03.3/test/com/entertainment/TelevisionTest.java
@@ -0,0 +1,67 @@
+package com.entertainment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TelevisionTest {
+
+  private Television television;
+
+  @Before
+  public void setUp() throws Exception {
+    television=new Television("Sony", INITIAL_VOLUME, DisplayType.OLED);
+  }
+
+  @Test
+  public void getBrand() {
+  }
+
+  @Test
+  public void setBrand() {
+  }
+
+  @Test
+  public void getVolume() {
+  }
+
+  @Test
+  public void setVolume_valid() {
+    television.setVolume(Television.MIN_VOLUME+1);
+    assertEquals(INITIAL_VOLUME, television.getVolume());
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void setVolume_invalid() {
+    television.setVolume(Television.MIN_VOLUME - 1);
+  }
+
+  @Test
+  public void getCurrentChannel() {
+  }
+
+  @Test
+  public void changeChannel() {
+  }
+
+  @Test
+  public void getDisplay() {
+  }
+
+  @Test
+  public void setDisplay() {
+  }
+
+  @Test
+  public void compareTo() {
+  }
+
+  @Test
+  public void testHashCode() {
+  }
+
+  @Test
+  public void testEquals() {
+  }
+}
